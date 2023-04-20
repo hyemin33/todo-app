b6f9d5556a06b5a01eda0b2d0d710604dd092180
hyemin33 <joliie@naver.com>
2023-04-21 00:09:19 +0900

    feat: layout

---
+++ src/components/layouts/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Wrap = styled.div`
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  max-width: 960px;
+  margin: auto;
+  background: red;
+`;
