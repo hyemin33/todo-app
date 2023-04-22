a9c5de803ae1680543a735bca60ba5756bfb362b
hyemin33 <joliie@naver.com>
2023-04-22 14:46:36 +0900

    feat: add login

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,17 +3,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from '@src/pages/Main';
 import ErrorPage from '@src/pages/Error';
 import DefaultLayout from '@src/components/layouts';
+import LoginPage from '@src/pages/Login';
 
 export const AppRouter = () => {
   const router = createBrowserRouter([
     {
       element: <DefaultLayout />,
-
+      errorElement: <ErrorPage />,
       children: [
         {
           path: '/',
           element: <MainPage />,
-          errorElement: <ErrorPage />,
+        },
+        {
+          path: '/login',
+          element: <LoginPage />,
         },
       ],
     },
