031227bf043fddeb688be28f7c629d45187ca3ba
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2023-04-25 00:15:10 +0900

    fix: main page (#12)
    * feat: add signup page
    
    * fix: add signin button, signup button

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,17 @@
+import { FlexCenterBox, Box } from '@src/components/atoms/Box';
+import Button from '@src/components/atoms/Button';
+import { useNavigate } from 'react-router-dom';
+
 const MainPage = () => {
-  return <div>mainpage</div>;
+  const navigate = useNavigate();
+  return (
+    <FlexCenterBox>
+      <Box>
+        <Button text='로그인' onClick={() => navigate('/signin')} />
+        <Button text='회원가입' onClick={() => navigate('/signup')} />
+      </Box>
+    </FlexCenterBox>
+  );
 };
 
 export default MainPage;
