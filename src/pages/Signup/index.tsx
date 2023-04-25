091fe45a5e45b95117fd93814f46797ee7dea86b
hyemin33 <joliie@naver.com>
2023-04-25 21:41:34 +0900

    fix: signup page redirect

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,6 +1,16 @@
 import AuthForm from '@src/components/organism/AuthForm';
+import auth from '@src/utils/auth';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.getToken()) {
+      navigate('/todo');
+    }
+  }, []);
   return <AuthForm mode='signup' />;
 };
 
